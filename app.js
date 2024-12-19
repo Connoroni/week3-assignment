0ca2ed284af814afd91f32edfff46809239c6ebe
Connoroni <mitchell2k2@gmail.com>
2024-12-19 15:16:21 +0000

    fetched API and pushed to empty array

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 let gameData = {
   cookieCount: 0,
   cookiesPerSec: 0,
-  // cookiesPerClick: 0,
-  // upgrade1: 0,
-  // upgrade2: 0,
-  // upgrade3: 0,
-  // upgrade4: 0,
-  // upgrade5: 0,
+  //cookiesPerClick: 0,
+  //upgrade1: 0,
+  //upgrade2: 0,
+  //upgrade3: 0,
+  //upgrade4: 0,
+  //upgrade5: 0,
   // upgrade numbers are showing the total number the user has of each upgrade
 };
+//! Get gameData from local storage here
 
-// Get gameData from local storage here
-
-// Fetch the API
+const shopUpgradesArray = [];
+async function getShopUpgrades() {
+  const result = await fetch(
+    "https://cookie-upgrade-api.vercel.app/api/upgrades"
+  );
+  //   console.log(result);
+  const resultJson = await result.json();
+  //   console.log(resultJson);
+  //   const resultWrangled = resultJson[0];
+  //   return resultWrangled;
+  //I was wrong, no wrangling needed
+  shopUpgradesArray.push(resultJson);
+  console.log(shopUpgradesArray);
+}
+// getShopUpgrades();
