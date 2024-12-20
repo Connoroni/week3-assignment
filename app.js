45e98713281cb3d7bef3035385c5221de7f44154
Connoroni <mitchell2k2@gmail.com>
2024-12-20 10:50:36 +0000

    found the issue with the renderUpgrades function and finally got it working

--- app.js
+++ app.js
@@ -2,16 +2,21 @@ let gameData = {
   cookieCount: 0,
   cookiesPerSec: 0,
   //cookiesPerClick: 0,
-  //upgrade1: 0,
-  //upgrade2: 0,
-  //upgrade3: 0,
-  //upgrade4: 0,
-  //upgrade5: 0,
+  upgrade1: 0,
+  upgrade2: 0,
+  upgrade3: 0,
+  upgrade4: 0,
+  upgrade5: 0,
+  upgrade6: 0,
+  upgrade7: 0,
+  upgrade8: 0,
+  upgrade9: 0,
+  upgrade10: 0,
   // upgrade numbers are showing the total number the user has of each upgrade
 };
 //! Get gameData from local storage here
 
-const shopUpgradesArray = [];
+const upgradesArray = [];
 async function getShopUpgrades() {
   const result = await fetch(
     "https://cookie-upgrade-api.vercel.app/api/upgrades"
@@ -22,7 +27,36 @@ async function getShopUpgrades() {
   //   const resultWrangled = resultJson[0];
   //   return resultWrangled;
   //I was wrong, no wrangling needed
-  shopUpgradesArray.push(resultJson);
-  console.log(shopUpgradesArray);
+  upgradesArray.push(resultJson);
+  console.log(upgradesArray);
+  renderUpgrades();
 }
-// getShopUpgrades();
+getShopUpgrades();
+
+//You would not believe how long it took me to realise that my problem was that the .push makes an array within another array so each item is actually upgradesArray[0][i]
+
+const shopBox = document.getElementById("shopBox");
+async function renderUpgrades() {
+  // const getItems = await getShopUpgrades();
+  // getItems.forEach(function (upgrade) {
+  await getShopUpgrades;
+  for (let i = 0; i < upgradesArray[0].length; i++) {
+    console.log(i);
+    const upgradeContainer = document.createElement("div");
+    upgradeContainer.className = "shop-item";
+    upgradeContainer.id = `upgrade${i}`;
+    shopBox.appendChild(upgradeContainer);
+    //? let upgradeAmount = document.createElement("p");
+    //? upgradeAmount.textContent = gameData
+    let upgradeName = document.createElement("p");
+    upgradeName.textContent = upgradesArray[0][i].name;
+    upgradeContainer.appendChild(upgradeName);
+    let upgradeCost = document.createElement("p");
+    upgradeCost.textContent = upgradesArray[0][i].cost;
+    upgradeContainer.appendChild(upgradeCost);
+    let upgradeIncrease = document.createElement("p");
+    upgradeIncrease.textContent = upgradesArray[0][i].increase;
+    upgradeContainer.appendChild(upgradeIncrease);
+    console.log(upgradeIncrease);
+  }
+}
